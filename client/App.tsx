a4dd3b8dfb9b498a01b3a5d4a2ba5d01617a93b2
pugal202 <pugalshanmugam0@gmail.com>
2026-09-04 16:00:59 +0000

    Add Journey Intelligence disruption recovery workflow

--- client/App.tsx
+++ client/App.tsx
@@ -19,6 +19,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/journey-intelligence" element={<Index initialSection="Journey Intelligence" />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
